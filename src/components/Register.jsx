52550d570aad0d2f32be83cff22784bf6c509f51
Jongmin Kim <jongmin3725@gmail.com>
2025-07-11 20:54:41 +1000

    fiex sign up error

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,9 +6,11 @@ export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const API_URL = process.env.REACT_APP_API_URL;
+
 
   const handleRegister = () => {
-    fetch("http://localhost:5000/register", {
+    fetch(`${API_URL}/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
